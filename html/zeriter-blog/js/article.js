f5e5502b35845543adb7f6c4c0427e8e3ab8d0e2
ZeriterZhang <791825185@qq.com>
2020-06-15 22:06:23 +0800

    新建项目

--- html/zeriter-blog/js/article.js
+++ html/zeriter-blog/js/article.js
@@ -13,6 +13,33 @@ var loadPage = {
                if(data.code ==200){
                 $("#articleTime").html("<i class=\"calendar icon\"></i>"+formatTime(data.data.blog.createDate));
                 $("#articleEye").html("<i class=\"eye icon\"></i>"+data.data.blog.click);
+                if(data.data.blog.blogType=="1"){
+                    $("#articleType").html("原创")
+                }else if(data.data.blog.blogType=="2"){
+                    $("#articleType").html("转载")
+                }else if(data.data.blog.blogType=="3"){
+                    $("#articleType").html("翻译")
+                }
+                $("#articleTitle").html(decodeURIComponent(data.data.blog.blogTitle ));
+                
+                $("#test-editormd-view2").html("<textarea id=\"append-test\" style=\"display:none;\">"+decodeURIComponent(data.data.blog.blogContent)+"</textarea>"   );
+                var show, 
+                show = editormd.markdownToHTML("test-editormd-view2", {
+                    htmlDecode      : "style,script,iframe",  // you can filter tags decode
+                    emoji           : true,
+                    taskList        : true,
+                    tex             : true,  // 默认不解析
+                    flowChart       : true,  // 默认不解析
+                    sequenceDiagram : true,  // 默认不解析
+                });
+                // tags
+                var itemHtml = ""
+                data.data.tags.forEach(element => {
+                    time = element.createDate;
+                    itemHtml = itemHtml +
+                       "<div class=\"ui  basic teal left pointing label\" >"+element.tagName+"</div>"
+                });
+                $("#tags").html(itemHtml)
                }
             }
         })
